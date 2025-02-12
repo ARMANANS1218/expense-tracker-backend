f714973ac19c26d995a4cd55b340d0ffe420acbb
ARMAN ANSARI <1by20et013@bmsit.in>
2025-02-13 01:57:58 +0530

    done

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// app.js
+/* eslint-env node */
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -18,5 +18,5 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/expenses', require('./routes/expenses'));
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
